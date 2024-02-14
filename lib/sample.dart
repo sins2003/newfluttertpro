4bb92536351b5fc49efe4ea4288e24f58154d416
Sinsarul haque v k <sinsarulhaq31@gmail.com>
2024-02-14 11:47:36 +0530

    commit

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner:false,
-      home: sample(),));
+    debugShowCheckedModeBanner: false,
+    home: sample(),
+  ));
 }
-class sample extends StatelessWidget{
+
+class sample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("welcome",style:TextStyle(color: Colors.white),),backgroundColor: Colors.red,),
-      body: Container(decoration: BoxDecoration(gradient: LinearGradient(colors: [
-      Colors.blue,
-      Colors.yellow,
-      Colors.blue,
-      ]
-      ,begin: Alignment.bottomCenter
-      ,end: Alignment.topCenter)),
+      appBar: AppBar(
+        title: Text(
+          "welcome",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg1.jpg"), fit: BoxFit.fill)),
         width: double.infinity,
         height: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.ice_skating_sharp,color: Colors.black,size: 50,),
+            Icon(
+              Icons.ice_skating_sharp,
+              color: Colors.black,
+              size: 50,
+            ),
             Image(image: AssetImage("assets/images/game png.png")),
-            Text("flutter",style: TextStyle(color: Colors.green,fontWeight: FontWeight.w800,fontSize: 20,),),
+            Text(
+              "flutter",
+              style: TextStyle(
+                color: Colors.green,
+                fontWeight: FontWeight.w800,
+                fontSize: 20,
+              ),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
