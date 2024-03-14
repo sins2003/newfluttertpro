ea7b0b0a554987df08d0fe8e8d6d2fe93c59fb4f
Sinsarul haque v k <sinsarulhaq31@gmail.com>
2024-03-14 11:05:22 +0530

    commit

--- lib/Bottom Navigations/example1.dart
+++ lib/Bottom Navigations/example1.dart
@@ -15,8 +15,8 @@ class naviexample1 extends StatefulWidget {
 }
 
 class _naviexample1State extends State<naviexample1> {
-  // var index=1;
-  // var scrn = [naviexample1(), Text("Cart"), Text("Account")];
+  var index=1;
+  var scrn = [naviexample1(), Text("Cart"), Text("Account")];
 
   @override
   Widget build(BuildContext context) {
@@ -59,115 +59,118 @@ class _naviexample1State extends State<naviexample1> {
           ),
         ),
       ),
-      body:
-      Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 50,
-              child: Wrap(
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 70, right: 50, top: 15),
-                    child: Container(
-                      height: 20,
-                      width: 90,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.blue,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "VEGITABLES",
-                          style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: Wrap(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 70, right: 50, top: 15),
+                      child: Container(
+                        height: 20,
+                        width: 90,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "VEGITABLES",
+                            style: TextStyle(color: Colors.green),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: Container(
-                      height: 20,
-                      width: 90,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.blue,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "FRUITS",
-                          style: TextStyle(color: Colors.white),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Container(
+                        height: 20,
+                        width: 90,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "FRUITS",
+                            style: TextStyle(color: Colors.green),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(right: 50, left: 50, top: 15),
-                    child: Container(
-                      height: 20,
-                      width: 90,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.blue,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "EXOTIC",
-                          style: TextStyle(color: Colors.white),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 50, left: 50, top: 15),
+                      child: Container(
+                        height: 20,
+                        width: 90,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "EXOTIC",
+                            style: TextStyle(color: Colors.green),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 250,
-              child: CarouselSlider(
-                options: CarouselOptions(
-                    height: double.infinity,
-                    aspectRatio: 16 / 9,
-                    viewportFraction: 0.8,
-                    initialPage: 0,
-                    enableInfiniteScroll: true,
-                    reverse: false,
-                    autoPlay: true,
-                    autoPlayInterval: Duration(seconds: 5),
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enlargeCenterPage: true,
-                    enlargeFactor: 0.2,
-                    scrollDirection: Axis.horizontal),
-                items: [
-                  Image(image: AssetImage("assets/images/londen.jpg")),
-                  Image(image: AssetImage("assets/images/portugal.jpg")),
-                  Image(image: AssetImage("assets/images/rocky mountain.jpg")),
-                  Image(image: AssetImage("assets/images/delhi.jpg")),
-                ],
+              Container(
+                width: double.infinity,
+                height: 250,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                      height: double.infinity,
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.8,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      reverse: false,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 5),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                      enlargeFactor: 0.2,
+                      scrollDirection: Axis.horizontal),
+                  items: [
+                    Image(image: AssetImage("assets/images/londen.jpg")),
+                    Image(image: AssetImage("assets/images/portugal.jpg")),
+                    Image(
+                        image: AssetImage("assets/images/rocky mountain.jpg")),
+                    Image(image: AssetImage("assets/images/delhi.jpg")),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 100,
-              color: Colors.blue,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 310),
-              child: Text(
-                "Shop By Category",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 20),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.blue,
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 310),
+                child: Text(
+                  "Shop By Category",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20),
+                ),
+              ),
+              Container()
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -175,7 +178,8 @@ class _naviexample1State extends State<naviexample1> {
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.cart), label: "Cart"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.cart), label: "Cart"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
         ],
       ),
