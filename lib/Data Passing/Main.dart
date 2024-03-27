898e422762513992700180269d80bd62159ca428
Sinsarul haque v k <sinsarulhaq31@gmail.com>
2024-03-27 16:45:22 +0530

    commit

---
+++ lib/Data Passing/Main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:newfluttertpro/Data%20Passing/2ndscreen.dart';
+import 'package:newfluttertpro/Data%20Passing/datas.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: datapassingmain(),
+    routes: {'secondpage': (context) => productdetail()},
+  ));
+}
+
+class datapassingmain extends StatefulWidget {
+  @override
+  State<datapassingmain> createState() => _datapassingmainState();
+}
+
+class _datapassingmainState extends State<datapassingmain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: DummyDatas.map((product) => GestureDetector(
+              child: Center(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text("${product["name"]}"),
+                      Image(
+                        image: AssetImage("${product["image"]}"),
+                        height: 100,
+                        width: 100,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap:() => getinfo(context,product){},
+            )).toList(),
+      ),
+    );
+  }
+}
---
+++ lib/Data Passing/datas.dart
@@ -0,0 +1,23 @@
+List<Map<String,dynamic>> DummyDatas=[
+  {
+   "id":1,
+   "name":"Laptop",
+   "description":"ASUS Tuf dash f15 i5 12450h RTX 3050 16GB Ram 512GB SSD",
+    "image":"assets/images/Laptop.jpg",
+    "price":"830000"
+  },
+  {
+    "id":2,
+    "name":"Mouse",
+    "description":"Razer DeathAdder Essential White Edition - 6400 DPI Ergonomic Wired Gaming Mouse - RZ01-03850200-R3M1",
+    "image":"assets/images/Mouse.jpg",
+    "price":"30000"
+  },
+  {
+    "id":3,
+    "name":"Monitor",
+    "description":"Samsung 43-inch(108cm) Odyssey Neo G7 UHD, 144Hz, 1 ms, Flat Gaming Monitor, Smart TV, Gaming Hub, Quantum Mini LED, HDR600, Game Bar, AMD FreeSync Premium Pro, (LS43CG700NWXXL, Black)",
+    "image":"assets/images/Monitor.jpg",
+    "price":"830000"
+  }
+];
